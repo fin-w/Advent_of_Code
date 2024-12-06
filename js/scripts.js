8987336c137924c7e95733ef873ac52e7f4b63c0
fin-w <fin-w@tutanota.com>
2024-12-06 13:36:10 +0000

    day 4 complete

--- js/scripts.js
+++ js/scripts.js
@@ -130,3 +130,175 @@ function runDay3Challenge2() {
 
     result.innerHTML = total;
 }
+
+function* directionsIterator() {
+    yield 'North';
+    yield 'NorthEast';
+    yield 'East';
+    yield 'SouthEast';
+    yield 'South';
+    yield 'SouthWest';
+    yield 'West';
+    yield 'NorthWest';
+}
+
+function day4CountXmasMatches(i, j, dataByLine) {
+    let mPos = [i, j];
+    let aPos = [i, j];
+    let sPos = [i, j];
+    let totalMatchesAroundPos = 0;
+    const maxWidth = dataByLine[0].length;
+    const maxHeight = dataByLine.length;
+
+    const directions = directionsIterator();
+    for (const direction of directions) {
+        switch (direction) {
+            case 'North':
+                mPos = [i, j - 1];
+                aPos = [i, j - 2];
+                sPos = [i, j - 3];
+                break;
+            case 'NorthEast':
+                mPos = [i + 1, j - 1];
+                aPos = [i + 2, j - 2];
+                sPos = [i + 3, j - 3];
+                break;
+            case 'East':
+                mPos = [i + 1, j];
+                aPos = [i + 2, j];
+                sPos = [i + 3, j];
+                break;
+            case 'SouthEast':
+                mPos = [i + 1, j + 1];
+                aPos = [i + 2, j + 2];
+                sPos = [i + 3, j + 3];
+                break;
+            case 'South':
+                mPos = [i, j + 1];
+                aPos = [i, j + 2];
+                sPos = [i, j + 3];
+                break;
+            case 'SouthWest':
+                mPos = [i - 1, j + 1];
+                aPos = [i - 2, j + 2];
+                sPos = [i - 3, j + 3];
+                break;
+            case 'West':
+                mPos = [i - 1, j];
+                aPos = [i - 2, j];
+                sPos = [i - 3, j];
+                break;
+            case 'NorthWest':
+                mPos = [i - 1, j - 1];
+                aPos = [i - 2, j - 2];
+                sPos = [i - 3, j - 3];
+                break;
+            default:
+                throw new Error("Direction value not recognised");
+        }
+
+        // if the required position of the chars goes beyond the puzzle area, ignore this entry
+        if (mPos[0] < 0 || mPos[0] >= maxWidth
+            || mPos[1] < 0 || mPos[1] >= maxHeight
+            || aPos[0] < 0 || aPos[0] >= maxWidth
+            || aPos[1] < 0 || aPos[1] >= maxHeight
+            || sPos[0] < 0 || sPos[0] >= maxWidth
+            || sPos[1] < 0 || sPos[1] >= maxHeight) {
+            continue;
+        }
+
+        if (dataByLine[mPos[0]].charAt(mPos[1]) === "M"
+            && dataByLine[aPos[0]].charAt(aPos[1]) === "A"
+            && dataByLine[sPos[0]].charAt(sPos[1]) === "S") {
+            totalMatchesAroundPos += 1;
+        }
+    }
+    return totalMatchesAroundPos;
+}
+
+function runDay4Challenge1() {
+    const data = document.getElementById("day4Data");
+    const dataByLine = data.innerText.split("\n")
+    let result = document.getElementById("day4Challenge1Result");
+    let totalMatches = 0;
+
+    for (let i = 0; i < dataByLine.length; i++) {
+        for (let j = 0; j < dataByLine[0].length; j++) {
+            if (dataByLine[i][j] === "X") {
+                totalMatches += day4CountXmasMatches(i, j, dataByLine);
+            }
+        }
+    }
+
+    result.innerHTML = totalMatches;
+}
+
+function day4IsMasInX(i, j, dataByLine) {
+    const maxWidth = dataByLine[0].length;
+    const maxHeight = dataByLine.length;
+
+    // ensure the area to search is within the puzzle area
+    if (i < 1 || i >= maxWidth || j < 1 || j + 1 >= maxHeight) {
+        return false;
+    }
+
+    // NW, SE, SW, NE
+    let surroundingPos = [[i - 1, j - 1], [i + 1, j + 1], [i - 1, j + 1], [i + 1, j - 1]];
+    let nwToSeMatch = false;
+    let swToNeMatch = false;
+
+    // NW to SE
+    switch (dataByLine[surroundingPos[0][1]].charAt(surroundingPos[0][0])) {
+        case "M":
+            if (dataByLine[surroundingPos[1][1]].charAt(surroundingPos[1][0]) === "S") {
+                nwToSeMatch = true;
+            }
+            break;
+        case "S":
+            if (dataByLine[surroundingPos[1][1]].charAt(surroundingPos[1][0]) === "M") {
+                nwToSeMatch = true;
+            }
+            break;
+        default:
+            break;
+    }
+
+    // SW to NE
+    switch (dataByLine[surroundingPos[2][1]].charAt(surroundingPos[2][0])) {
+        case "M":
+            if (dataByLine[surroundingPos[3][1]].charAt(surroundingPos[3][0]) === "S") {
+                swToNeMatch = true;
+            }
+            break;
+        case "S":
+            if (dataByLine[surroundingPos[3][1]].charAt(surroundingPos[3][0]) === "M") {
+                swToNeMatch = true;
+            }
+            break;
+        default:
+            break;
+    }
+
+    if (nwToSeMatch && swToNeMatch) {
+        return true;
+    }
+    return false;
+}
+
+function runDay4Challenge2() {
+    const data = document.getElementById("day4Data");
+    const dataByLine = data.innerText.split("\n")
+    let result = document.getElementById("day4Challenge2Result");
+    let totalMatches = 0;
+
+    for (let j = 0; j < dataByLine.length; j++) {
+        for (let i = 0; i < dataByLine[0].length; i++) {
+            if (dataByLine[j][i] === "A" && day4IsMasInX(i, j, dataByLine)) {
+                totalMatches += 1;
+            }
+        }
+    }
+
+    result.innerHTML = totalMatches;
+}
+
